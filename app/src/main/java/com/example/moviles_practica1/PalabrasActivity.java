80f7691798033a77b4144f5ea2e5bd8db5ca68b0
orlandoacort <orlandoacort@gmail.com>
2019-03-17 23:02:04 -0500

    + metodo Palindromas

--- app/src/main/java/com/example/moviles_practica1/PalabrasActivity.java
+++ app/src/main/java/com/example/moviles_practica1/PalabrasActivity.java
@@ -15,6 +15,9 @@ import java.util.Arrays;
 
 public class PalabrasActivity extends AppCompatActivity {
 
+    ListView lista ;
+    String listas[] = {"1","2","3","4","5"};
+    private static int contador=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +26,22 @@ public class PalabrasActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String palabras= intent.getStringExtra(MainActivity.EXTRA_STRING);
         String cpalabras = contarPalabras(palabras);
+        //String[] prepetidas = palabrasRepetidas(palabras);
 
 
+       ///////CANTIDAD DE PALABRAS
         TextView textView = (TextView) findViewById(R.id.idResultado);
+        textView.setText("TOTAL DE PALABRAS: " + cpalabras);
 
-        textView.setText("Cantidad de palabras: " + cpalabras);
 
+        lista = (ListView)findViewById(R.id.simpleLV);
+        ArrayAdapter<String> arrayAdapter  = new ArrayAdapter<String> (this,R.layout.list_view,R.id.TvLv,palindromas(palabras)  );/*listas palabrasRepetidas(cpalabras)*/
+        lista.setAdapter(arrayAdapter);
+
+        String cPalindromas = String.valueOf(contador); //C. DE PALINDROMAS
+        //////CANTIDAD DE PALABRAS PALINDROMAS
+        TextView textView2 = (TextView) findViewById(R.id.idResPalindromas);
+        textView2.setText("TOTAL DE PALINDROMAS: " + cPalindromas);
 
 
 
@@ -46,21 +59,7 @@ public class PalabrasActivity extends AppCompatActivity {
 
     }
 
-  /*  public static String contarPalabras(String s){
-        int contador = 1 , pos;
-        s = s.trim();
-        if (s.isEmpty()){
-            contador = 0;
-        }
-        else {
-            pos = s.indexOf(" ");
-            while (pos != -1) {
-                contador++;
-                pos = s.indexOf(" ", pos + 1);
-            }
-        }
-        return String.valueOf(contador) ;
-    }*/
+
 
     public static String contarPalabras(String p){
 
@@ -68,10 +67,53 @@ public class PalabrasActivity extends AppCompatActivity {
         return String.valueOf(palabras.length) ;
     }
 
-    public static String[] palabrasRepetidas(String p){
+    public static String[] guardarPalabras(String p){
+
+        String palabras[] = p.split(" ");
+        return palabras;
+    }
+
+    public static String[] palindromas(String p){
+
+        String  reverso;
+        String palabra[]= p.split(" ");
+        String palResult[] = new String[palabra.length]  ;
+        for(int i =0;i<palabra.length;i++){
+            reverso = "";
+            String original = palabra[i];
+
+            int length = original.length();
+
+            for (int j=length-1;j>=0;j--){
+                reverso = reverso + original.charAt(j);
+
+
+            }
+
+            if (original.equalsIgnoreCase(reverso)){
+                palResult[i] = original + " Es palindroma " + "\n";
+                contador++;
+            }
+            else {
+               // palResult[i] = original + " NO es palindroma" + "\n";
+                palResult[i] = "";
+            }
+
+
+
+        }
+
+
+        return palResult;
+    }
+
+
+    ////////////////////////////////////////////////////////
+   /* public static String[] palabrasRepetidas(String p){
         String palabras [] = p.split(" ");
         String palabrasAyu [] = p.split(" ");
         int contador[] = new int[Integer.parseInt(contarPalabras(p))-1];
+        String Scont[] = new String[Integer.parseInt(contarPalabras(p))-1];
 
         for(int i=0; i< Integer.parseInt(contarPalabras(p)) ; i++ ){
             contador[i] = 0;
@@ -80,18 +122,18 @@ public class PalabrasActivity extends AppCompatActivity {
             for(int j=0;j<Integer.parseInt(contarPalabras(p)); j++){
                 if (palabra.equalsIgnoreCase(palabrasAyu[j])){
                     contador[i]++;
+
                     palabras[j]= "";
                 }
 
             }
-
+            Scont[i] = String.valueOf(contador[i]) ;
         }
 
 
-        return new String[]{Arrays.toString(contador)};
+        return palabras;
     }
-
-    // CREAR ITEM
+*/
 
 
 
